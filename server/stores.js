b5ef7fa2506d063335f1f7de8e09d2042338f79a
Roberto Barbero <rbarbero@gmail.com>
2016-04-28 14:06:52 +0100

    added bug to the stores page

--- server/stores.js
+++ server/stores.js
@@ -8,6 +8,7 @@ var db = require('./pghelper'),
  * @param next
  */
 
+/** Delete this comment line for the Store page to work (1/2)
 
 function findAll(req, res, next) {
     db.query("SELECT id, name, location__latitude__s AS latitude, location__longitude__s AS longitude FROM salesforce.store__c ORDER BY lastmodifieddate DESC")
@@ -19,3 +20,6 @@ function findAll(req, res, next) {
 
 
 exports.findAll = findAll;
+
+Delete this comment for the Store page to work (2/2) */
+
