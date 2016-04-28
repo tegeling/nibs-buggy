da8965627e1cbd97ea94e85c433464da9890b11b
Roberto Barbero <rbarbero@gmail.com>
2016-04-28 14:14:38 +0100

    added bug to the stores page

--- server/stores.js
+++ server/stores.js
@@ -8,7 +8,10 @@ var db = require('./pghelper'),
  * @param next
  */
 
-
+// ****************************************************************
+// A BUG HAS BEEN INTRODUCED on purpose to demonstrate Heroku flows
+// Delete 'WHERE name='bug' in the SELECT to fix
+// ****************************************************************
 
 function findAll(req, res, next) {
     db.query("SELECT id, name, location__latitude__s AS latitude, location__longitude__s AS longitude FROM salesforce.store__c WHERE name='bug' ORDER BY lastmodifieddate DESC")
